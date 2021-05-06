ed2fcdf6be448aee7b32a77a43b74f37612f9e66
ruoking-ww <1756067101@qq.com>
2021-05-06 15:01:25 +0800

    third commit

--- src/main/java/ruoking/git/GitTest.java
+++ src/main/java/ruoking/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hallo");
         System.out.println("hihihihi");
+        System.out.println("nihao");
     }
 }
